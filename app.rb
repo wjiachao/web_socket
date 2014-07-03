e1f3da769e256a3db753f16065ac2cdc0396eb55
Daneil Wei <daneilmu1@gmail.com>
2014-07-03 11:29:54 +0800

    add rerun

--- app.rb
+++ app.rb
@@ -10,43 +10,46 @@ EventMachine.run do
   end
   @clients = []
 
-  # EM::WebSocket.run(:host => "0.0.0.0", :port => 3001) do |ws|
-  #   ws.onopen { |handshake|
-  #     puts "WebSocket connection open"
+  EM::WebSocket.run(:host => "localhost", :port => 3001) do |ws|
+    ws.onopen { |handshake|
+      puts "WebSocket connection open"
+      
+      ws.ping(body = 'daneil') if ws.pingable?
+      # Access properties on the EM::WebSocket::Handshake object, e.g.
+      # path, query_string, origin, headers
 
-  #     # Access properties on the EM::WebSocket::Handshake object, e.g.
-  #     # path, query_string, origin, headers
+      # Publish message to the client
+      ws.send "Hello Client, you connected to #{handshake.path}"
+    }
 
-  #     # Publish message to the client
-  #     ws.send "Hello Client, you connected to #{handshake.path}"
-  #   }
+    ws.onclose { puts "Connection closed" }
 
-  #   ws.onclose { puts "Connection closed" }
-
-  #   ws.onmessage { |msg|
-  #     puts "Recieved message: #{msg}"
-  #     ws.send "Pong: #{msg}"
-  #   }
-  # end
-  EM::WebSocket.start(:host => '0.0.0.0', :post => '3001') do |ws|
-    ws.onopen do |handshake|
-      @clients << ws
-      puts "WebSocket Connection open"
-      ws.send "Connected to #{handshake.path}."
-    end
-
-    ws.onclose do 
-      ws.send "closed."
-      @clients.delete ws
-    end
-
-    ws.onmessage do |msg|
-      puts "Received Message: #{msg}"
-      @clients.each do |socket|
-        socket.send msg
-      end
-    end
+    ws.onmessage { |msg|
+      puts "Recieved message: #{msg}"
+      #ws.send "Pong: #{msg}"
+      ws.pong(body = 'daneil')
+    }
   end
+  # EM::WebSocket.start(:host => 'localhost', :post => '3001') do |ws|
+  #   ws.onopen do |handshake|
+  #     puts ws.pingable?
+  #     @clients << ws
+  #     puts "WebSocket Connection open"
+  #     ws.send "Connected to #{handshake.path}."
+  #   end
+
+  #   ws.onclose do 
+  #     ws.send "closed."
+  #     @clients.delete ws
+  #   end
+
+  #   ws.onmessage do |msg|
+  #     puts "Received Message: #{msg}"
+  #     @clients.each do |socket|
+  #       socket.send msg
+  #     end
+  #   end
+  # end
   # our WebSockets server logic will go here
 
   App.run! :port => 3000
